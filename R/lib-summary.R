c0fe4aa66b6244c00930db28e4b4e4c5e6a7c583
Andy Teucher <andy.teucher@gmail.com>
2023-08-02 19:00:00 -0700

    Add sizes parameter to lib_summary, use fs pkg

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -8,10 +8,21 @@
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if (sizes) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1)
+    )
+  }
+
   pkg_df
 }
