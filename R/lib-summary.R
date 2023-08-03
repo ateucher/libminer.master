5c2007c541bf62093e89c266e5582f17b0c7ec21
Andy Teucher <andy.teucher@gmail.com>
2023-08-02 18:48:15 -0700

    document lib_summary()

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,13 @@
+#' R Library Summary
+#'
+#' Provides a brief summary of the package libraries on your machine
+#'
+#' @return A data.frame containing the count of packages in each of the user's
+#'   libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
